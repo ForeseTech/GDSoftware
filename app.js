bcc0d44eabbb789d02b25965d19549fd2563e2ad
Nilesh D <nileshlund@gmail.com>
2021-01-06 22:12:03 +0530

    feat: Use EJS and EjsMate to render views and create landing page

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
+const ejsMate = require('ejs-mate');
 const Database = require('./config/database');
 
 const app = express();
+
+// Views and View Engine
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views/'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 const PORT = process.env.PORT || 3000;
 

b065d00fe088cb9783e0827ea9fdfdfb44e5d91b
Nilesh D <nileshlund@gmail.com>
2021-01-11 16:58:54 +0530

    fix: Update Database Schema

--- models/Student.js
+++ models/Student.js
@@ -1,27 +1,44 @@
 const mongoose = require('mongoose');
 
-const StudentSchema = mongoose.Schema({
-  registerNum: {
-    type: Number,
-    required: true,
-  },
+const StudentSchema = mongoose.Schema(
+  {
+    registerNum: {
+      type: Number,
+      required: true,
+    },
 
-  name: {
-    type: String,
-    required: true,
-  },
+    name: {
+      type: String,
+      required: true,
+    },
 
-  department: {
-    type: String,
-    enum: ['AUT', 'BIO', 'CHE', 'CIV', 'CSE', 'ECE', 'EEE', 'INT', 'MEC'],
-  },
+    department: {
+      type: String,
+      enum: ['AUT', 'BIO', 'CHE', 'CIV', 'CSE', 'ECE', 'EEE', 'INT', 'MEC'],
+    },
 
-  member: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Member',
-    required: true,
+    scores: {
+      type: Object,
+      required: true,
+    },
+
+    comments: {
+      type: String,
+    },
+
+    gdCompleted: {
+      type: Boolean,
+      default: false,
+    },
+
+    member: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Member',
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Student = mongoose.model('Student', StudentSchema, 'Students');
 module.exports = Student;

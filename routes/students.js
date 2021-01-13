b165f0fc0a7371689e0930012e864232d595b9da
Nilesh D <nileshlund@gmail.com>
2021-01-13 14:44:54 +0530

    fix: Add middleware to check if member is logged in before performing actions

--- routes/students.js
+++ routes/students.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const { isLoggedIn } = require('../middleware');
+
 const { createStudent, updateStudent, deleteStudent } = require('../controllers/students');
 
-router.route('/new').post(createStudent);
-router.route('/:id').put(updateStudent).delete(deleteStudent);
+router.route('/new').post(isLoggedIn, createStudent);
+router.route('/:id').put(isLoggedIn, updateStudent).delete(isLoggedIn, deleteStudent);
 
 module.exports = router;

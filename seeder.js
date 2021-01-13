5d0af913b2374a09609d76e00a3f3f87489b0743
Nilesh D <nileshlund@gmail.com>
2021-01-13 23:19:31 +0530

    fix: Seeder script and sample dataset

--- seeder.js
+++ seeder.js
@@ -6,14 +6,17 @@ const Database = require('./config/database');
 
 // Load models
 const Student = require('./models/Student');
+const Member = require('./models/Member');
 
 // Read JSON files
 const students = JSON.parse(fs.readFileSync(`${__dirname}/resources/_data/students.json`, 'utf-8'));
+const members = JSON.parse(fs.readFileSync(`${__dirname}/resources/_data/members.json`, 'utf-8'));
 
 // Import into DB
 const importData = async () => {
   try {
     await Student.create(students);
+    await Member.create(members);
 
     console.log('Data Imported...'.green);
     process.exit();
@@ -26,6 +29,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Student.deleteMany();
+    await Member.deleteMany();
 
     console.log('Data Destroyed...'.red);
     process.exit();

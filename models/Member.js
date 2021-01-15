5aaebb104bca60390720e51d699429f604f7ce83
Nilesh D <nileshlund@gmail.com>
2021-01-15 21:54:55 +0530

    feat: Helper methods for logging in and registration

--- models/Member.js
+++ models/Member.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const MemberSchema = mongoose.Schema({
   name: {
@@ -20,6 +21,7 @@ const MemberSchema = mongoose.Schema({
     type: String,
     required: true,
     minlength: 6,
+    select: false,
   },
 });
 
@@ -28,6 +30,18 @@ MemberSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Sign JWT and return
+MemberSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Match user entered password to hashed password in database
+MemberSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const Member = mongoose.model('Member', MemberSchema, 'Members');
 module.exports = Member;
